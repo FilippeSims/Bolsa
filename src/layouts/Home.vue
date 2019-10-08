d1c7ef233e61a6394070a2d7d1a70cb33d49ba63
Filippe <filippesims@gmail.com>
2019-10-08 06:38:20 -0300

    Bolsa

---
+++ src/layouts/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Bolsa App
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  // name: 'LayoutName',
+
+  data () {
+    return {
+      leftDrawer: false
+    }
+  }
+}
+</script>
